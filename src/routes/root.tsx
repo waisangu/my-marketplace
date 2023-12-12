f8b01f679fd8664e7b8412a3a9689ed585971960
Wai San Gu <waisangu@gmail.com>
2023-12-11 22:33:48 -0500

    added destroy route

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -3,16 +3,20 @@ import {
   Link, 
   useLoaderData, 
   Form,
+  redirect,
+  NavLink,
+  useNavigation,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export async function action() {
   const contact = await createContact();
-  return { contact };
+  return redirect(`/contacts/${contact.id}/edit`);
 }
 
 export default function Root() {
   const { contacts } = useLoaderData();
+  const navigation = useNavigation();
     return (
       <>
         <div id="sidebar">
@@ -45,7 +49,12 @@ export default function Root() {
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
+                    <NavLink
+                      to={`contacts/${contact.id}`}
+                      className={({ isActive, isPending }) =>
+                        isActive ? "active" : isPending ? "pending" : ""
+                      }
+                    >
                       {contact.first || contact.last ? (
                         <>
                           {contact.first} {contact.last}
@@ -54,7 +63,7 @@ export default function Root() {
                         <i>No Name</i>
                       )}{" "}
                       {contact.favorite && <span>★</span>}
-                    </Link>
+                    </NavLink>
                   </li>
                 ))}
               </ul>
@@ -65,7 +74,11 @@ export default function Root() {
             )}
           </nav>
         </div>
-        <div id="detail">
+        <div id="detail"
+          className={
+            navigation.state === "loading" ? "loading" : ""
+          }
+        >
           <Outlet />
         </div>
       </>
